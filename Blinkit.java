4b18732d412c7c33a800d6cfa147fac5c48b3e6b
azharali87901 <kutagullaazharali>
2024-10-07 15:04:00 +0530

    Zomato Application Create SuccessFully...

---
+++ Blinkit.java
@@ -0,0 +1,9 @@
+abstract class Blinkit
+{
+	public void displayAllProducts()
+	{
+		System.out.println("Display All Products.........");	
+	}
+	public void addProductTocart(int id);
+}
+kutagullaazharali
---
+++ Zomato.java
@@ -0,0 +1,11 @@
+class Zomato
+{
+	public static void zomatoApplication()
+	{
+		System.out.println("welcome to Zomato Application");
+	}
+	public static void main(String []args)
+	{
+		zomatoApplication();
+	}
+}
